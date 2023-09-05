7baf9288de74833d7fa41954e652d0bb48b6783d
Zachary O'Brien <zachary.obrien@mymail.champlain.edu>
2023-09-05 11:28:04 -0400

    Builds successfully, no white triangle yet

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -5,8 +5,45 @@
 #include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
 
+
+
+// Creates a new shader of a given type.
+// Possible types: GL_VERTEX_SHADER, GL_FRAGMENT_SHADER, etc
+// Returns id of the shader object
+unsigned int createShader(GLenum shaderType, const char* sourceCode);
+
+
+//Creates a new vertex array object with vertex data
+unsigned int createVAO(float* vertexData, int numVertices);
+
+
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
+
+
+//global variable in main.cpp, or in main()
+float vertices[9] = {
+	//x   //y  //z
+	-0.5, -0.5, 0.0, //Bottom left
+	 0.5, -0.5, 0.0, //Bottom right
+	 0.0,  0.5, 0.0  //Top center
+};
+
+const char* vertexShaderSource = R"(
+#version 450
+layout(location = 0) in vec3 vPos;
+void main(){
+gl_Position = vec4(vPos,1.0);
+}
+)";
+
+const char* fragmentShaderSource = R"(
+#version 450
+out vec4 FragColor;
+void main(){
+FragColor = vec4(1.0);
+}
+)";
 
 int main() {
 	printf("Initializing...");
@@ -27,6 +64,39 @@ int main() {
 		return 1;
 	}
 
+	//Define a new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	//Tell vao to pull vertex data from vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//Define position attribute (3 floats)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+	//Create a new vertex shader object
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(vertexShader);
+
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+	}
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
@@ -34,4 +104,14 @@ int main() {
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
+}
+
+unsigned int createShader(GLenum shaderType, const char* sourceCode)
+{
+	return 0;
+}
+
+unsigned int createVAO(float* vertexData, int numVertices)
+{
+	return 0;
 }
